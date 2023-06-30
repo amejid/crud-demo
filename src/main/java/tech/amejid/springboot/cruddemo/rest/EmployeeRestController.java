ff66beaf65e503ba62552fc58b4a8d82744e85f2
amejid <abdelmejid89@gmail.com>
2023-06-30 15:20:29 +0300

    Set up REST controller

---
+++ src/main/java/tech/amejid/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,24 @@
+package tech.amejid.springboot.cruddemo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.amejid.springboot.cruddemo.dao.EmployeeDAO;
+import tech.amejid.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+}
