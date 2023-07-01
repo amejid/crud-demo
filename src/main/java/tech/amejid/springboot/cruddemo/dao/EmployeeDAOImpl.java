576f4096580c0ea24fe85eca4d57aefabb96c1ee
amejid <abdelmejid89@gmail.com>
2023-07-01 15:05:48 +0300

    Implement CRUD methods in DAO

--- src/main/java/tech/amejid/springboot/cruddemo/dao/EmployeeDAOImpl.java
+++ src/main/java/tech/amejid/springboot/cruddemo/dao/EmployeeDAOImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 public class EmployeeDAOImpl implements EmployeeDAO {
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
 
     @Autowired
     public EmployeeDAOImpl(EntityManager entityManager) {
@@ -21,8 +21,22 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     public List<Employee> findAll() {
         TypedQuery<Employee> query = entityManager.createQuery("FROM Employee", Employee.class);
 
-        List<Employee> employees = query.getResultList();
+        return query.getResultList();
+    }
 
-        return employees;
+    @Override
+    public Employee findById(int id) {
+        return entityManager.find(Employee.class, id);
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return entityManager.merge(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Employee employee = entityManager.find(Employee.class, id);
+        entityManager.remove(employee);
     }
 }
