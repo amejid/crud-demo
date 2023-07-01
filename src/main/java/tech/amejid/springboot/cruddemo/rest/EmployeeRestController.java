2f61be0f338cc321fa42f4e5ebe9877643728d31
amejid <abdelmejid89@gmail.com>
2023-07-01 15:33:22 +0300

    Implement REST CRUD methods

--- src/main/java/tech/amejid/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/tech/amejid/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,10 +1,7 @@
 package tech.amejid.springboot.cruddemo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import tech.amejid.springboot.cruddemo.dao.EmployeeDAO;
+import org.springframework.web.bind.annotation.*;
 import tech.amejid.springboot.cruddemo.entity.Employee;
 import tech.amejid.springboot.cruddemo.service.EmployeeService;
 
@@ -23,5 +20,37 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        employee.setId(0);
+
+        return employeeService.save(employee);
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        return employeeService.save(employee);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public void deleteEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        employeeService.deleteById(employeeId);
     }
 }
